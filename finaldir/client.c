480c1233ab88171a731e8686829b529629d08e80
jjh030325 <wpqlks7@gmail.com>
2024-06-15 01:44:57 +0900

    imsi2

--- finaldir/client.c
+++ finaldir/client.c
@@ -6,47 +6,45 @@
 #include<sys/socket.h>
 #include<pthread.h>
 #include<time.h>
- 
+
 #define BUF_SIZE 100
 #define NORMAL_SIZE 20
- 
+
 void* send_msg(void* arg);
 void* recv_msg(void* arg);
 void error_handling(char* msg);
- 
-char name[NORMAL_SIZE]="[DEFALT]";     // name
-char msg_form[NORMAL_SIZE];            // msg form
-char msg[BUF_SIZE];                    // msg
-char serv_port[NORMAL_SIZE];        // server port number
- 
+
+char name[NORMAL_SIZE] = "[DEFAULT]"; // name
+char msg[BUF_SIZE]; // msg
+
 int main(int argc, char *argv[])
 {
     int sock;
     struct sockaddr_in serv_addr;
     pthread_t snd_thread, rcv_thread;
     void* thread_return;
- 
-    if (argc!=4)
+
+    if (argc != 4)
     {
-        printf(" Usage : %s <ip> <port> <name>\n", argv[0]);
+        printf("Usage : %s <ip> <port> <name>\n", argv[0]);
         exit(1);
     }
-	
-	sprintf(name, "%s", argv[3]); 
-    sock=socket(PF_INET, SOCK_STREAM, 0);
- 
-    memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family=AF_INET;
-    serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
-    serv_addr.sin_port=htons(atoi(argv[2]));
- 
-    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1)
-        error_handling(" conncet() error");
 
-	printf("client starts.\n");
-	printf("your name: %s\n", name);
-	printf("Connection successful.\n");
- 
+    sprintf(name, "[%s]", argv[3]);
+    sock = socket(PF_INET, SOCK_STREAM, 0);
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_addr.sin_port = htons(atoi(argv[2]));
+
+    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+        error_handling("connect() error");
+
+    printf("Client starts.\n");
+    printf("Your name: %s\n", name);
+    printf("Connection successful.\n");
+
     pthread_create(&snd_thread, NULL, send_msg, (void*)&sock);
     pthread_create(&rcv_thread, NULL, recv_msg, (void*)&sock);
     pthread_join(snd_thread, &thread_return);
@@ -54,50 +52,51 @@ int main(int argc, char *argv[])
     close(sock);
     return 0;
 }
- 
+
 void* send_msg(void* arg)
 {
-    int sock=*((int*)arg);
-    char name_msg[NORMAL_SIZE+BUF_SIZE+1];
-    char myInfo[BUF_SIZE];
-    char* who = NULL;
-    char temp[BUF_SIZE];
- 
-    /** send join messge **/
-    sprintf(myInfo, "--- 새로운 사용자 %s이 들어왔습니다. --- \n", name);
-    write(sock, myInfo, strlen(myInfo));
- 
-    while(1)
+    int sock = *((int*)arg);
+    char name_msg[NORMAL_SIZE + BUF_SIZE + 1];
+
+    // 클라이언트가 접속했음을 서버에 알림
+    snprintf(name_msg, sizeof(name_msg), "--- %s has joined ---\n", name);
+    write(sock, name_msg, strlen(name_msg));
+
+    while (1)
     {
         fgets(msg, BUF_SIZE, stdin);
- 
-        // send message
-        sprintf(name_msg, "%s] %s", name, msg);
+        if (!strcmp(msg, "q\n") || !strcmp(msg, "Q\n"))
+        {
+            close(sock);
+            exit(0);
+        }
+        snprintf(name_msg, sizeof(name_msg), "%s %s", name, msg);
         write(sock, name_msg, strlen(name_msg));
     }
     return NULL;
 }
- 
+
 void* recv_msg(void* arg)
 {
-    int sock=*((int*)arg);
-    char name_msg[NORMAL_SIZE+BUF_SIZE];
+    int sock = *((int*)arg);
+    char name_msg[NORMAL_SIZE + BUF_SIZE];
     int str_len;
- 
-    while(1)
+
+    while (1)
     {
-        str_len=read(sock, name_msg, NORMAL_SIZE+BUF_SIZE-1);
-        if (str_len==-1)
+        str_len = read(sock, name_msg, NORMAL_SIZE + BUF_SIZE - 1);
+        if (str_len == -1)
             return (void*)-1;
-        name_msg[str_len]=0;
+        name_msg[str_len] = 0;
         fputs(name_msg, stdout);
     }
     return NULL;
 }
- 
+
 void error_handling(char* msg)
 {
     fputs(msg, stderr);
     fputc('\n', stderr);
     exit(1);
 }
+
