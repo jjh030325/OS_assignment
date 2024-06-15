487a6282422c1c2c7464fae30ea48daa3e2b9816
jjh030325 <wpqlks7@gmail.com>
2024-06-15 21:05:35 +0900

    스레드풀

--- finaldir/server.c
+++ finaldir/server.c
@@ -10,6 +10,7 @@
 #define BUF_SIZE 100
 #define MAX_CLNT 100
 #define NORMAL_SIZE 20
+#define MAX_THREADS 10
 
 void *handle_clnt(void *arg);
 void send_msg(char *msg, int len);
@@ -18,13 +19,14 @@ void error_handling(char *msg);
 int clnt_cnt = 0;
 int clnt_socks[MAX_CLNT];
 pthread_mutex_t mutx;
+pthread_cond_t cond;
 
 int main(int argc, char *argv[])
 {
     int serv_sock, clnt_sock;
     struct sockaddr_in serv_adr, clnt_adr;
     socklen_t clnt_adr_sz;
-    pthread_t t_id;
+    pthread_t threads[MAX_THREADS];
 
     if (argc != 2)
     {
@@ -33,6 +35,7 @@ int main(int argc, char *argv[])
     }
 
     pthread_mutex_init(&mutx, NULL);
+    pthread_cond_init(&cond, NULL);
     serv_sock = socket(PF_INET, SOCK_STREAM, 0);
 
     memset(&serv_adr, 0, sizeof(serv_adr));
@@ -48,81 +51,92 @@ int main(int argc, char *argv[])
     printf("Server starts..\n");
     printf("Waiting for clients.\n");
 
-    while (1)
-    {
-        clnt_adr_sz = sizeof(clnt_adr);
-        clnt_sock = accept(serv_sock, (struct sockaddr *)&clnt_adr, &clnt_adr_sz);
-
-        pthread_mutex_lock(&mutx);
-        clnt_socks[clnt_cnt++] = clnt_sock;
-        pthread_mutex_unlock(&mutx);
-
-        pthread_create(&t_id, NULL, handle_clnt, (void *)&clnt_sock);
-        pthread_detach(t_id);
+    for (int i = 0; i < MAX_THREADS; ++i) {
+        pthread_create(&threads[i], NULL, handle_clnt, (void *)&serv_sock);
     }
+
+    for (int i = 0; i < MAX_THREADS; ++i) {
+        pthread_join(threads[i], NULL);
+    }
+
     close(serv_sock);
     return 0;
 }
 
 void *handle_clnt(void *arg)
 {
-    int clnt_sock = *((int *)arg);
-    int str_len = 0, i;
-    char msg[BUF_SIZE];
-    char name[NORMAL_SIZE];
+    int serv_sock = *((int *)arg);
+    int clnt_sock;
+    struct sockaddr_in clnt_adr;
+    socklen_t clnt_adr_sz = sizeof(clnt_adr);
 
-    // 클라이언트가 연결되었을 때 이름을 읽음
-    if ((str_len = read(clnt_sock, name, sizeof(name) - 1)) != 0)
+    while (1)
     {
-        name[str_len] = '\0'; // 이름 끝에 널 문자 추가
-
-        // 새로운 클라이언트가 연결되었음을 모든 클라이언트에게 알림
-        char welcome_msg[BUF_SIZE];
-        sprintf(welcome_msg, "--- 새로운 사용자 %s이 들어왔습니다. ---\n", name);
-        send_msg(welcome_msg, strlen(welcome_msg));
-
-        // 서버 콘솔에 출력
-        printf("client %d: %s joined.\n", clnt_sock-3, name);
-    }
-
-    while ((str_len = read(clnt_sock, msg, sizeof(msg) - 1)) != 0)
-    {
-        msg[str_len] = '\0'; // 읽어온 데이터 끝에 널 문자 추가
-        send_msg(msg, str_len); // 클라이언트로부터 받은 메시지를 모든 클라이언트에게 전송
-        printf("%s\n", msg); // 서버 콘솔에 클라이언트가 보낸 메시지 출력
-    }
-
-    // 클라이언트가 연결을 끊었을 때
-    pthread_mutex_lock(&mutx);
-    for (i = 0; i < clnt_cnt; i++)
-    {
-        if (clnt_sock == clnt_socks[i])
+        pthread_mutex_lock(&mutx);
+        while (clnt_cnt >= MAX_CLNT)
         {
-            // 현재 클라이언트를 클라이언트 배열에서 제거하고 나머지 클라이언트들을 앞으로 이동
-            for (int j = i; j < clnt_cnt - 1; j++)
-                clnt_socks[j] = clnt_socks[j + 1];
-            break;
+            pthread_cond_wait(&cond, &mutx);
         }
+        pthread_mutex_unlock(&mutx);
+
+        clnt_sock = accept(serv_sock, (struct sockaddr *)&clnt_adr, &clnt_adr_sz);
+
+        pthread_mutex_lock(&mutx);
+        clnt_socks[clnt_cnt++] = clnt_sock;
+        pthread_cond_signal(&cond);
+        pthread_mutex_unlock(&mutx);
+
+        pthread_detach(pthread_self()); // 스레드를 분리하여 자원 해제
+
+        // 클라이언트 처리 함수 호출
+        // 여기서부터는 클라이언트와의 통신을 처리
+        char name[NORMAL_SIZE];
+        int str_len = read(clnt_sock, name, sizeof(name) - 1);
+        if (str_len > 0)
+        {
+            name[str_len] = '\0';
+            char welcome_msg[BUF_SIZE];
+            sprintf(welcome_msg, "--- 새로운 사용자 %s이(가) 들어왔습니다. ---\n", name);
+            send_msg(welcome_msg, strlen(welcome_msg));
+            printf("client %d: %s joined.\n", clnt_sock-3, name);
+        }
+
+        char msg[BUF_SIZE];
+        while ((str_len = read(clnt_sock, msg, sizeof(msg))) != 0)
+        {
+            msg[str_len] = '\0';
+            send_msg(msg, str_len);
+            printf("%s\n", msg);
+        }
+
+        pthread_mutex_lock(&mutx);
+        for (int i = 0; i < clnt_cnt; i++)
+        {
+            if (clnt_sock == clnt_socks[i])
+            {
+                for (int j = i; j < clnt_cnt - 1; j++)
+                    clnt_socks[j] = clnt_socks[j + 1];
+                break;
+            }
+        }
+        clnt_cnt--;
+        pthread_mutex_unlock(&mutx);
+
+        close(clnt_sock);
+
+        char leave_msg[BUF_SIZE];
+        sprintf(leave_msg, "--- %s님이 나갔습니다. ---\n", name);
+        send_msg(leave_msg, strlen(leave_msg));
+        printf("client %d: %s left.\n", clnt_sock-3, name);
     }
-    clnt_cnt--;
-    pthread_mutex_unlock(&mutx);
-    close(clnt_sock);
-
-    // 클라이언트가 나갔음을 모든 클라이언트에게 알림
-    sprintf(msg, "--- %s님이 나갔습니다. ---\n", name);
-    send_msg(msg, strlen(msg));
-
-    // 서버 콘솔에 출력
-    printf("client %d: %s left.\n", clnt_sock-3, name);
 
     return NULL;
 }
 
 void send_msg(char *msg, int len)
 {
-    int i;
     pthread_mutex_lock(&mutx);
-    for (i = 0; i < clnt_cnt; i++)
+    for (int i = 0; i < clnt_cnt; i++)
         write(clnt_socks[i], msg, len);
     pthread_mutex_unlock(&mutx);
 }
@@ -133,4 +147,3 @@ void error_handling(char *msg)
     fputc('\n', stderr);
     exit(1);
 }
-
